0d00bdbe24a328ffb4667f3074bb153be18c0a74
aryamaansingh <aryamaan.singh@orchids.edu.in>
2023-08-05 13:12:09 +0530

    Initialising project

---
+++ Source/UE503PluginExp/UE503PluginExp.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE503PluginExp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE503PluginExp, "UE503PluginExp" );
---
+++ Source/UE503PluginExp/UE503PluginExpGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE503PluginExpGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE503PLUGINEXP_API AUE503PluginExpGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
